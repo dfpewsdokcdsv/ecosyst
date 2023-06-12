1c48ec7b5e3fbd4f25534a6f232c269c4c4e8cf8
dfpewsdokcdsv <132742180+dfpewsdokcdsv@users.noreply.github.com>
2023-06-12 12:48:26 +0600

    Update facts.py

--- facts.py
+++ facts.py
@@ -2,9 +2,12 @@ import random
 from bot_logic import gen_pass
 
 def gen_pass(facts):
-    elements = "+-/*!&$#?=@<>"
+    elements = "1. Найди людное место и попробуй поискать мусора. Не забудь взять пакет! 2. Попробуй пойти вместе со своими друзьями.",
+                  "Вместе интересней.",
+                  "3. Если вы найдете какие-нибудь батарейки, несите их на переработку."
+    facts = ""
     gen_pass(10)
-    for i in range(pass_length):
-        password += random.choice(elements)
+    for i in range(facts):
+        facts += random.choice(elements)
 
-    return password
+    return facts
