04c51a5407a4ca3f19fbdd7c1016b49f99abc20e
dfpewsdokcdsv <132742180+dfpewsdokcdsv@users.noreply.github.com>
2023-06-18 23:00:18 +0600

    Update facts.py

--- facts.py
+++ facts.py
@@ -1,12 +1,1 @@
-import random
-from bot_logic import gen_pass
 
-def gen_pass(facts):
-    elements = "1. Найди людное место и попробуй поискать мусора. Не забудь взять пакет! 2. Попробуй пойти вместе со своими друзьями. Вместе интересней.",
-                  "3. Если вы найдете какие-нибудь батарейки, несите их на переработку."
-    facts = ""
-    gen_pass(10)
-    for i in range(facts):
-        facts += random.choice(elements)
-
-    return facts
